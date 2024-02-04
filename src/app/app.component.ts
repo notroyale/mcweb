0f937bf3104d2c1d13fc119d2f68202ad6f02ac0
Rokas Palionis <devlt@inbox.lt>
2024-02-05 00:37:22 +0200

    roadmap added, some design bugs fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { InfoGridComponent } from "./shared/info-grid/info-grid.component";
 import { CardsGridComponent } from "./shared/cards-grid/cards-grid.component";
+import { TokenomicsSectionComponent } from "./shared/tokenomics-section/tokenomics-section.component";
+import { RoadmapSectionComponent } from "./shared/roadmap-section/roadmap-section.component";
+import { IntroCardsComponent } from "./shared/intro-cards/intro-cards.component";
 const menu = document.querySelector(".menu-block");
 
 @Component({
@@ -10,7 +13,7 @@ const menu = document.querySelector(".menu-block");
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, InfoGridComponent, CardsGridComponent]
+    imports: [CommonModule, RouterOutlet, InfoGridComponent, CardsGridComponent, TokenomicsSectionComponent, RoadmapSectionComponent, IntroCardsComponent]
 })
 export class AppComponent {
   title = 'mcweb';
---
+++ src/app/shared/tokenomics-section/tokenomics-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tokenomics-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './tokenomics-section.component.html',
+  styleUrl: './tokenomics-section.component.scss'
+})
+export class TokenomicsSectionComponent {
+
+}
