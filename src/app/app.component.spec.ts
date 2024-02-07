31e46152d6d2d098075f123c1b9bdcb4cff77949
Rokas Palionis <devlt@inbox.lt>
2024-02-08 01:51:23 +0200

    small merge fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sol' title`, () => {
+  it(`should have the 'mcweb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sol');
+    expect(app.title).toEqual('mcweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sol');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mcweb');
   });
 });
