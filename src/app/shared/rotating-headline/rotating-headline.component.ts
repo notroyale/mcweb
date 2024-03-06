24576e99475b5b27157c16b17b4dec13e3136126
Rokas Palionis <devlt@inbox.lt>
2024-03-06 21:57:09 +0200

    moving text fixed

--- src/app/shared/rotating-headline/rotating-headline.component.ts
+++ src/app/shared/rotating-headline/rotating-headline.component.ts
@@ -5,32 +5,37 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './rotating-headline.component.html',
-  styleUrl: './rotating-headline.component.scss'
+  styleUrl: './rotating-headline.component.scss',
 })
 export class RotatingHeadlineComponent implements OnInit {
-
-
-
-
   ngOnInit(): void {
     setInterval(() => {
-      const show = document.querySelector('span[data-show]')
+      const show = document.querySelector('span[data-show]');
       if (show) {
-              const next = show.nextElementSibling || document.querySelector('span:first-child')
-      const up = document.querySelector('span[data-up]')
-      
-      if (up) {
-        up.removeAttribute('data-up')
-      }
-      
-      show.removeAttribute('data-show')
-      show.setAttribute('data-up', '')
-      if(next) {
-              next.setAttribute('data-show', '')
-      }
+        var next =
+          show.nextElementSibling || document.querySelector('span:first-child');
+        const up = document.querySelector('span[data-up]');
+        if (up) {
+          up.removeAttribute('data-up');
+        }
 
-      }
+        show.removeAttribute('data-show');
+        show.setAttribute('data-up', '');
+        if (next != null && next != undefined && next.innerHTML != "") {
+          console.log(next.innerHTML);
+          next.setAttribute('data-show', '');
+        }
+        else {
+          var firstChild = document.querySelector('.mask')?.firstElementChild;
+          if(firstChild != null && firstChild != undefined ) {
+            console.log('firstChild', firstChild);
+                      next = firstChild;
+                      next.setAttribute('data-show', '');
 
+
+          }
+        }
+      }
     }, 3000);
   }
 }
