b81e4d40db30007ec7dcbd46c9eb02561d8988df
Rokas Palionis <devlt@inbox.lt>
2024-03-23 14:21:51 +0200

    roadmap updated, tokenomics slider updated

--- src/app/shared/tokenomics-section/tokenomics-section.component.ts
+++ src/app/shared/tokenomics-section/tokenomics-section.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { throwError } from 'rxjs';
+
+export interface PhotosApi {
+  albumId?: number;
+  id?: number;
+  title?: string;
+  url?: string;
+  thumbnailUrl?: string;
+}
 
 @Component({
   selector: 'app-tokenomics-section',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule,CommonModule],
   templateUrl: './tokenomics-section.component.html',
   styleUrl: './tokenomics-section.component.scss'
 })
-export class TokenomicsSectionComponent {
+export class TokenomicsSectionComponent   {
+
+  
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    center: true,
+    dots: false,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      }
+    }
+  }
+  
+
+
+
+
 
 }
